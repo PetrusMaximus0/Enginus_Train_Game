9b06e708f50eb1324e40b27965da0b19cdbdcc9f
MaximusDesktop <5740112+PetrusMaximus0@users.noreply.github.com>
2023-12-09 13:51:15 +0100

    update

--- Components/SpriteComponent.h
+++ Components/SpriteComponent.h
@@ -16,11 +16,11 @@ private:
 	float Heading{};
 
 public:
-
 	void SetTexture(const char* TexturePath);
 	void SetSourceRectangle(int X, int Y, int W = GAME_OBJECT_WIDTH, int H = GAME_OBJECT_HEIGHT);
 	void SetDestinationRectangle(int X, int Y, int W = GAME_OBJECT_WIDTH, int H = GAME_OBJECT_HEIGHT);
 	void RenderSprite();
 	void SetHeading(float Value);
+
 };
 
